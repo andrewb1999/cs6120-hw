cf6c0b619d642821eb6837fc62c6dd6144ebb409
Andrew Butt <andrewb1999@gmail.com>
2022-02-01 20:40:14 -0500

    Working add_count in rust

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cs6120-lesson-2
+
+Must first run:
+```
+git submodule init
+git submodule update --init --recursive
+```
---
+++ add_count/src/main.rs
@@ -0,0 +1,25 @@
+use bril_rs::load_abstract_program;
+use bril_rs::AbstractCode;
+use bril_rs::AbstractInstruction;
+
+fn is_add(instr : &AbstractInstruction) -> i32 {
+    match instr {
+        AbstractInstruction::Value { op, .. } if op == "add" => 1,
+        _ => 0
+    }
+}
+
+fn main() {
+    let program = load_abstract_program();
+    let mut num_adds = 0;
+    for func in program.functions {
+        for i in func.instrs {
+            let add = match &i {
+                AbstractCode::Instruction(instr) => is_add(instr),
+                _ => 0
+            };
+            num_adds += add;
+        }
+    }
+    println!("Program contains {} add instructions", num_adds)
+}

a07ae3bd5f4601b49aba6c7067f87f1de5b84282
Andrew Butt <andrewb1999@gmail.com>
2022-02-04 18:37:30 -0500

    Global tdce

--- lesson3/tdce/src/form_blocks.rs
+++ lesson3/tdce/src/form_blocks.rs
@@ -1,8 +1,8 @@
-use bril_rs::{AbstractInstruction, AbstractProgram, AbstractCode};
+use bril_rs::{AbstractInstruction, AbstractFunction, AbstractCode};
 
 #[derive(Default)]
-pub struct Block {
-    pub instrs: Vec<AbstractInstruction>,
+pub struct Block<'a> {
+    pub instrs: Vec<&'a AbstractCode>,
 }
 
 fn get_op(instr : &AbstractInstruction) -> String {
@@ -22,25 +22,23 @@ fn is_terminator(instr : &AbstractInstruction) -> bool {
     }
 }
 
-fn get_code(program : AbstractProgram) -> Vec<AbstractCode> {
+fn get_code(func : &AbstractFunction) -> Vec<&AbstractCode> {
     let mut code = Vec::new();
-    for func in program.functions {
-        for instr in func.instrs {
-            code.push(instr)
-        }
+    for instr in &func.instrs {
+        code.push(instr)
     }
     code
 }
 
-pub fn form_blocks<'a>(program : AbstractProgram) -> Vec<Block>{
-    let instrs = get_code(program);
+pub fn form_blocks(func : &AbstractFunction) -> Vec<Block>{
+    let instrs = get_code(func);
     let mut blocks = Vec::new();
     let mut cur_block : Block = Default::default();
     for i in instrs {
         match i {
             AbstractCode::Instruction(instr) => {
                 let term = is_terminator(&instr);
-                cur_block.instrs.push(instr);
+                cur_block.instrs.push(i);
                 if term {
                     blocks.push(cur_block);
                     cur_block = Default::default();
@@ -51,6 +49,7 @@ pub fn form_blocks<'a>(program : AbstractProgram) -> Vec<Block>{
                     blocks.push(cur_block);
                     cur_block = Default::default()
                 }
+                cur_block.instrs.push(i)
             },
         }
     }
@@ -58,4 +57,34 @@ pub fn form_blocks<'a>(program : AbstractProgram) -> Vec<Block>{
         blocks.push(cur_block)
     }
     blocks
+}
+
+pub fn print_blocks(blocks : &Vec<Block>) {
+    println!("----------------------");
+    for block in blocks {
+        for instr in &block.instrs {
+            println!("{}", instr)
+        }
+        println!("----------------------");
+    }
+}
+
+pub fn print_func_blocks(func : &AbstractFunction) {
+    println!("----------------------");
+    for block in form_blocks(func) {
+        for instr in block.instrs {
+            println!("{}", instr)
+        }
+        println!("----------------------");
+    }
+}
+
+pub fn flatten_blocks(blocks : Vec<Block>) -> Vec<AbstractCode> {
+    let mut instrs = Vec::new();
+    for block in blocks {
+        for instr in block.instrs {
+            instrs.push(instr.clone());
+        }
+    }
+    instrs
 }
--- lesson3/tdce/src/main.rs
+++ lesson3/tdce/src/main.rs
@@ -2,50 +2,60 @@ pub mod form_blocks;
 pub use form_blocks::*;
 
 use std::collections::HashSet;
-use bril_rs::AbstractCode;
-use bril_rs::{load_abstract_program, output_abstract_program};
-use bril_rs::AbstractInstruction;
-use bril_rs::AbstractProgram;
+use bril_rs::*;
 
-// fn trivial_global_dce_pass(prog : &AbstractProgram) -> bool {
-//     let mut used = HashSet::new();
-//     for func in &prog.functions {
-//         for code in &func.instrs {
-//             match code {
-//                 AbstractCode::Instruction(instr) =>
-//                     match instr {
-//                         AbstractInstruction::Value {args, ..} => used.extend(args),
-//                         AbstractInstruction::Effect {args, ..} => used.extend(args),
-//                         _ => (),
-//                     },
-//                 _ => (),
-//             }
-//         }
-//     }
+fn trivial_global_dce_pass(func : &mut AbstractFunction) -> bool {
+    let mut blocks = form_blocks(&func);
+    let mut used = HashSet::new();
+    for block in &blocks {
+        for code in &block.instrs {
+            match code {
+                AbstractCode::Instruction(instr) =>
+                    match instr {
+                        AbstractInstruction::Value {args, ..} => used.extend(args),
+                        AbstractInstruction::Effect {args, ..} => used.extend(args),
+                        _ => (),
+                    },
+                _ => (),
+            }
+        }
+    }
+
+    let mut done = false;
     
-//     for func in &prog.functions{
-//         for code in &func.instrs {
-//             match code {
-//                 AbstractCode::Instruction(instr) =>
-//                     match instr {
-//                         AbstractInstruction::Constant {dest, ..} 
-//                         if !used.contains(dest) => 
-                            
-//                     }
-//             }
-//         }
-//     }
-//     false
-// }
+    for i in 0..blocks.len() {
+        let block = &blocks[i];
+        let mut new_block = Block { instrs: Vec::new() };
+        for code in &block.instrs {
+            match code {
+                AbstractCode::Instruction(instr) =>
+                    match instr {
+                        AbstractInstruction::Constant {dest, ..} | AbstractInstruction::Value {dest, ..} => {
+                            if !used.contains(dest) {
+                                done = true
+                            } else {
+                                new_block.instrs.push(code)
+                            }
+                        },
+                        AbstractInstruction::Effect {..} => new_block.instrs.push(code),
+                    }
+                AbstractCode::Label {..} => new_block.instrs.push(code),
+            }
+        }
+        blocks[i] = new_block;
+    }
+    func.instrs = flatten_blocks(blocks);
+    done
+}
+
+fn trivial_dce(func : &mut AbstractFunction) {
+    while trivial_global_dce_pass(func) {}
+}
 
 fn main() {
-    let program = load_abstract_program();
-    let blocks = form_blocks(program);
-    for block in blocks {
-        for instr in block.instrs {
-            println!("{}", instr)
-        }
-        println!("-----------")
+    let mut program = load_abstract_program();
+    for func in &mut program.functions {
+        trivial_dce(func);
     }
-    // while trivial_global_dce_pass(&program) {}
+    output_abstract_program(&program)
 }
